6d609ac87caed1f6dab314d90ec9dfd77bb51e18
jhamadhav <jhamadhav28@gmail.com>
2023-07-17 19:17:28 +0530

    project section added

---
+++ project.json.js
@@ -0,0 +1,18 @@
+let all_project_data = [
+    {
+        "name": "elemental Quests",
+        "description": "An innovative experience that blends the thrill of a treasure hunt with the power of storytelling and puzzles to provide a one-of-a-kind soft skills assessment experience.",
+        "tags": ["firebase", "nodejs"],
+        "github-link": "https://github.com/jhamadhav/elementalQuests",
+        "project-link": "https://elemental-quests.glitch.me/",
+        "image-link": "./images/default-project-image.jpg",
+    },
+    {
+        "name": "elemental Quests",
+        "description": "An innovative experience that blends the thrill of a treasure hunt with the power of storytelling and puzzles to provide a one-of-a-kind soft skills assessment experience.",
+        "tags": ["firebase", "nodejs"],
+        "github-link": "https://github.com/jhamadhav/elementalQuests",
+        "project-link": "https://elemental-quests.glitch.me/",
+        "image-link": "./images/default-project-image.jpg",
+    }
+]
