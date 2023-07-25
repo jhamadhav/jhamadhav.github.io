b0dd77392468fff0333ecc1758a6f9781567bd45
jhamadhav <jhamadhav28@gmail.com>
2023-07-25 18:02:41 +0530

    achievements section init

---
+++ scripts/achievement.js
@@ -0,0 +1,11 @@
+let all_achievement = [
+    {
+        "event_name": "Cepheus'22 by IT GOA",
+        "event_place": "Online",
+        "description": "'Hack the Game' was an adrenaline-pumping hackathon held at IIT Goa's Cepheus event in 2022. The event celebrated innovation, coding talent, and a passion for gaming.",
+        "achievement": "Winner of event 'Hack The Games",
+        "icon_link": "https://drive.google.com/file/d/1_BJp6aBZoHLWDzVROJOoBIeurAcBTtDV/view?usp=drive_link",
+        "certificate_link": "https://drive.google.com/drive/u/0/folders/1DoeXMHhEvJmFG5g6SC1TGnqYwWLoc6mw",
+        "date": "2022/01/18"
+    }
+]
